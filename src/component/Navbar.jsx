3d53f488ced3e08d1ea0fbb6950522cd13a98d45
Ishwar korde <90470682+Ikorde1@users.noreply.github.com>
2024-02-28 14:18:38 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,8 +10,8 @@ function Navbar() {
                 <h2 className=' text-white  kaushan-font absolute text-4xl left-8'>Portfolio</h2>
                 <li className="hide hover:text-blue-600 cursor-pointer" onClick={() => navigate("/")}>Home</li>
                 <li className="hide hover:text-blue-600 cursor-pointer" onClick={() => navigate("/projects")}>Projects</li>
-                <li className="hide hover:text-blue-600 cursor-pointer" onClick={() => navigate("/contact")}>Contact</li>
-                <li className="resume rounded  py-2 px-3 bg-lime-600 hover:bg-green-900 text-white"><a href="https://drive.google.com/uc?export=download&id=1V9TipLgVu6m7S_gENc8OJOu4pG3-joeh">Resume</a></li>
+                <li className="hide hover:text-blue-600 cursor-pointer" onClick={() => navigate("/Blogs")}>Blogs</li>
+                <li className="resume rounded text-center py-2 px-3 bg-lime-600 hover:bg-green-900 text-white"><a href="https://drive.google.com/uc?export=download&id=1V9TipLgVu6m7S_gENc8OJOu4pG3-joeh">Resume</a></li>
             </ul>
         </div>
     );
