ad0ad506b785d10d518bf436e8d6997a68bd642b
Ishwar korde <90470682+Ikorde1@users.noreply.github.com>
2024-02-28 14:17:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from '../src/component/Navbar'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
-import Contact from './pages/Contact'
+import Blogs from './pages/Blogs'
 import { Route,Routes,} from 'react-router-dom'
 
 export default function App(){
@@ -11,7 +11,7 @@ export default function App(){
   <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/Projects" element={<Projects/>} />
-        <Route path="/Contact" element={<Contact/>} />
+        <Route path="/Blogs" element={<Blogs/>} />
   </Routes>
   
   </div>)
